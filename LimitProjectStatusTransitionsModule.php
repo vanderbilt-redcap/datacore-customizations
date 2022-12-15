193b52dd6ed5dbba11006e332a422c8322017dc0
Mark McEver <mark@fastmail.net>
2022-12-15 12:51:03 -0600

    Finished limiting status transitions

--- LimitProjectStatusTransitionsModule.php
+++ LimitProjectStatusTransitionsModule.php
@@ -2,14 +2,57 @@
 
 class LimitProjectStatusTransitionsModule extends \ExternalModules\AbstractExternalModule
 {
-    function redcap_every_page_before_render(){
-        if(!$this->isSuperUser() && PAGE === 'ProjectGeneral/change_project_status.php'){
-            /**
-             * Block the status transition.
-             * This is a failsafe in case the UI modifications ever break after a future REDCap UI update.
-             */
-            echo 0; // Code 0 triggers an error message on the client side
-            $this->exitAfterHook();
+    function redcap_every_page_top(){
+        global $completed_time;
+
+        if(PAGE === 'ProjectSetup/other_functionality.php'){
+            ?>
+            <script>
+                (() => {
+                    if(<?=json_encode($this->isSuperUser())?>){
+                        return;
+                    }
+
+                    const start = Date.now();
+                    const intervalId = setInterval(() => {
+                        const elapsed = Date.now() - start
+                        const message = <?=json_encode($this->getProjectSetting('message'))?>;
+                        const methodOverrides = {}
+
+                        if(<?=json_encode($completed_time == '')?>){
+                            methodOverrides['markProjectAsCompleted'] = (original) => {
+                                simpleDialog(message)
+                            }
+                        }
+
+                        methodOverrides['btnMoveToProd'] = (original) => {
+                            if(<?=json_encode($this->getProjectStatus() === 'DEV')?>){
+                                original()
+                            }
+                            else{
+                                simpleDialog(message)
+                            }
+                        }
+
+                        if(window[Object.keys(methodOverrides)[0]] !== undefined || elapsed > 3000){
+                            clearInterval(intervalId)
+
+                            for(const [name, action] of Object.entries(methodOverrides)){
+                                const original = window[name]
+                                if(original === undefined){
+                                    alert('The Limit Project Status Transitions module could not find the ' + name + '() function!')
+                                    continue
+                                }
+
+                                window[name] = (...args) => action(() => {
+                                    original(...args)
+                                })
+                            }
+                        }
+                    }, 100)
+                })()
+            </script>
+            <?php
         }
     }
 }
