4c2760fbd539f2899a49dd46344e322443a35055
Mark McEver <mark@fastmail.net>
2023-02-21 09:52:32 -0600

    Allows logging of multiple records at once

--- save-new-time-logs.php
+++ save-new-time-logs.php
@@ -2,8 +2,14 @@
 
 $records = json_decode($_POST['data'], true);
 
+/**
+ * Record IDs should be ignored since 'addingAutoNumberedRecords' is true,
+ * but let's use negative record IDs just in case, so they can't possibly overlap with any actual entries.
+ */
+$nextRecordId = -1;
+
 foreach($records as &$record){
-    $record['record_id'] = -1;
+    $record['record_id'] = $nextRecordId--;
     $record['today_date'] = date('Y-m-d');
 }
 
